98c1311b85e1eb58a44621fe705b1077cb07242a
thecab002 <calebb2002@gmail.com>
2024-12-03 13:11:41 -0600

    Add files via upload

---
+++ ch12b/ch12b/scripts/script.js
@@ -0,0 +1,15 @@
+/*
+    Student Name: firstname lastname
+    File Name: script.js
+    Date: 01/01/2020
+*/
+
+//Hamburger menu function
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
